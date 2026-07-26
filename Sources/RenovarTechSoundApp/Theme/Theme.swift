5b4df002b68133d53e8ce6239d4cc66bd6ce6309
Cleiton1230 <66922424+Cleiton1230@users.noreply.github.com>
2026-07-26 16:35:09 -0300

    Create Theme.swift

---
+++ Sources/RenovarTechSoundApp/Theme/Theme.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+extension Color {
+    static let renovarTeal = Color(red: 0x49 / 255.0, green: 0x70 / 255.0, blue: 0x73 / 255.0)
+    static let renovarOrange = Color(red: 0xFF / 255.0, green: 0x33 / 255.0, blue: 0x00 / 255.0)
+    static let renovarBackground = Color(red: 0xF4 / 255.0, green: 0xF6 / 255.0, blue: 0xF6 / 255.0)
+}
